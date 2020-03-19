4e03849823400b85c5e1af63e6d2f4a6455b8d58
belikov_artem <dev.dreamvention@gmail.com>
2020-03-19 19:42:55 +0200

    убрать кредит

--- admin/controller/extension/module/paypal_smart_button.php
+++ admin/controller/extension/module/paypal_smart_button.php
@@ -54,7 +54,6 @@ class ControllerExtensionModulePayPalSmartButton extends Controller {
 		$data['text_pill'] = $this->language->get('text_pill');
 		$data['text_rect'] = $this->language->get('text_rect');
 		$data['text_checkout'] = $this->language->get('text_checkout');
-		$data['text_credit'] = $this->language->get('text_credit');
 		$data['text_pay'] = $this->language->get('text_pay');
 		$data['text_buy_now'] = $this->language->get('text_buy_now');
 		$data['text_pay_pal'] = $this->language->get('text_pay_pal');
