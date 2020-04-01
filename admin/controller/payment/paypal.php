799b9b79ffd434e2a67240df3bcf49c28261e8fa
belikov_artem <dev.dreamvention@gmail.com>
2020-04-02 02:00:22 +0300

    добавление токена

--- admin/controller/payment/paypal.php
+++ admin/controller/payment/paypal.php
@@ -257,7 +257,7 @@ class ControllerPaymentPayPal extends Controller {
 			$data['environment'] = 'production';
 		}
 				
-		$data['seller_nonce'] = token(50);
+		$data['seller_nonce'] = $this->token(50);
 		
 		$data['configure_url'] = array(
 			'production' => array(
@@ -486,5 +486,20 @@ class ControllerPaymentPayPal extends Controller {
 		}
 		
 		return !$this->error;
+	}
+	
+	private function token($length = 32) {
+		// Create random token
+		$string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	
+		$max = strlen($string) - 1;
+	
+		$token = '';
+	
+		for ($i = 0; $i < $length; $i++) {
+			$token .= $string[mt_rand(0, $max)];
+		}	
+	
+		return $token;
 	}
 }
