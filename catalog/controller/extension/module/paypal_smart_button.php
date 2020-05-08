4fe72131d171eafff8a17082a907f1f4232f0ee4
belikov_artem <dev.dreamvention@gmail.com>
2020-05-08 18:24:28 +0300

    правки

--- catalog/controller/extension/module/paypal_smart_button.php
+++ catalog/controller/extension/module/paypal_smart_button.php
@@ -2,6 +2,15 @@
 class ControllerExtensionModulePayPalSmartButton extends Controller {
 	private $error = array();
 		
+	public function __construct($registry) {
+		parent::__construct($registry);
+
+		if (version_compare(phpversion(), '7.1', '>=')) {
+			ini_set('precision', 14);
+			ini_set('serialize_precision', 14);
+		}
+	}
+	
 	public function index() {		
 		if ($this->config->get('payment_paypal_status') && isset($this->request->get['route'])) {						
 			$status = false;
