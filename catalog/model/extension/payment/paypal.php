98f8811adc2a53c821ef6777ed1ae084f919e4e0
belikov_artem <dev.dreamvention@gmail.com>
2025-01-21 19:12:49 +0200

    МУКЫШЩТ 3.1.14

--- catalog/model/extension/payment/paypal.php
+++ catalog/model/extension/payment/paypal.php
@@ -810,7 +810,7 @@ class ModelExtensionPaymentPayPal extends Model {
 			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "setting` WHERE store_id = '0' AND `key` = 'paypal_setting'");
 			
 			if ($query->row) {
-				$setting[$query->row['key']] = json_decode($query->row['value'], true);
+				$setting = json_decode($query->row['value'], true);
 			}
 						
 			$setting['paypal_setting']['general']['order_history_token'] = sha1(uniqid(mt_rand(), 1));
